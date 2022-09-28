18a434af7399e128902630f295c3c080eb44ac6b
ironhearts1 <jmanlambor@gmail.com>
2022-09-27 21:14:00 -0400

    first commit first lesson

---
+++ 01_output.php
@@ -0,0 +1,28 @@
+<?php
+   // echo "why won't live reload work :C";
+   /*
+   echo 1
+   echo 2
+   */
+  echo 123, "hello", 10.5; 
+  print 123;
+  print_r([1,2,3]);
+  var_dump("hello");
+  var_export("hello");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?='Post One';?></h1>
+</body>
+
+</html>
